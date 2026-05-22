fcd6c5b22b16d65776cec678eb263880dd828e74
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2026-05-22 13:57:48 -0500

    commited from wrong level xd, adding db changes

--- app-db/seed.sql
+++ app-db/seed.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    user_id integer PRIMARY KEY,
+    user_id UUID PRIMARY KEY,
     first_name varchar(255) NOT NULL,
     last_name varchar(255) NOT NULL,
     email varchar(255) NOT NULL UNIQUE,
@@ -8,14 +8,14 @@ CREATE TABLE users (
 );
 
 CREATE TABLE workspaces (
-    workspace_id integer PRIMARY KEY,
+    workspace_id UUID PRIMARY KEY,
     workspace_name varchar(255) NOT NULL,
-    owner_id integer REFERENCES users (user_id) ON DELETE CASCADE
+    owner_id UUID NOT NULL REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE documents (
-    document_id integer PRIMARY KEY,
-    workspace_id integer REFERENCES workspaces (workspace_id),
+    document_id UUID PRIMARY KEY,
+    workspace_id UUID NOT NULL REFERENCES workspaces (workspace_id),
     file_name varchar(255) NOT NULL,
     file_path varchar(255) NOT NULL,
     file_text text NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE documents (
 );
 
 CREATE TABLE automations (
-    automation_id integer PRIMARY KEY,
-    workspace_id integer REFERENCES workspaces (workspace_id),
+    automation_id UUID PRIMARY KEY,
+    workspace_id UUID NOT NULL REFERENCES workspaces (workspace_id),
     automation_name varchar(255) NOT NULL,
     trigger_type varchar(255) NOT NULL,
     action_type varchar(255) NOT NULL,
@@ -34,12 +34,9 @@ CREATE TABLE automations (
 );
 
 CREATE TABLE automation_logs (
-    automation_log_id integer PRIMARY KEY,
-    automation_id integer REFERENCES automations (automation_id),
+    automation_log_id UUID PRIMARY KEY,
+    automation_id UUID NOT NULL REFERENCES automations (automation_id),
     log_status varchar(255) NOT NULL,
     log_message text NOT NULL,
     created_at timestamp NOT NULL
 );
-
-INSERT INTO users VALUES (1, 'carlos','blanco','carlos@gmail.com','123abc','2026-07-02');
-
