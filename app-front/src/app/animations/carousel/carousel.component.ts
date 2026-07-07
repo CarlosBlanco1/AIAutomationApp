a3081d7237a256231fb9526d6de68db35241c28d
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2026-07-06 23:44:24 -0500

    forgot about home

--- app-front/src/app/animations/carousel/carousel.component.ts
+++ app-front/src/app/animations/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, viewChild } from '@angular/core';
+import { Component, effect, signal, viewChild } from '@angular/core';
 import { EmblaCarouselDirective } from 'embla-carousel-angular';
 import Autoplay from 'embla-carousel-autoplay';
 import type { EmblaCarouselType } from 'embla-carousel';
@@ -6,7 +6,7 @@ import type { EmblaCarouselType } from 'embla-carousel';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl : './carousel.component.css',
+  styleUrl: './carousel.component.css',
   imports: [EmblaCarouselDirective],
   standalone: true
 })
@@ -18,6 +18,8 @@ export class CarouselComponent {
 
   protected options = { loop: true };
 
+  isMobile = signal(window.innerWidth < 1024);
+
   protected plugins = [
     Autoplay({
       delay: 3000,
@@ -26,6 +28,10 @@ export class CarouselComponent {
   ];
 
   constructor() {
+    window.addEventListener('resize', () => {
+      this.isMobile.set(window.innerWidth < 1024);
+    });
+
     effect(() => {
       this.emblaApi = this.emblaRef()?.emblaApi;
     });
